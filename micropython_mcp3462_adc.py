644f7ea41a66fddc5e2939ca16981762151d6787
Jaiden Pant <jaiden.pant@planetinnovation.com.au>
2026-01-29 13:30:01 +1100

    DEG-3: Prepare for open source release and apply MIT license

--- micropython_mcp3462_adc.py
+++ micropython_mcp3462_adc.py
@@ -1,16 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-# PI Background IP
-# Copyright (c) 2021, Planet Innovation
+# MIT license; Copyright (c) 2021, Planet Innovation
 # 436 Elgar Road, Box Hill, 3128, VIC, Australia
 # Phone: +61 3 9945 7510
 #
-# The copyright to the computer program(s) herein is the property of
-# Planet Innovation, Australia.
-# The program(s) may be used and/or copied only with the written permission
-# of Planet Innovation or in accordance with the terms and conditions
-# stipulated in the agreement/contract under which the program(s) have been
-# supplied.
 
 from micropython import const
 
